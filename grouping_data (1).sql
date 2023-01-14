849cf41d962da78ca34d5fee7bb920e8de4742a2
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2023-01-14 22:51:07 +0600

    Update grouping_data (1).sql

--- grouping_data (1).sql
+++ grouping_data (1).sql
@@ -7,3 +7,16 @@ country
 FROM supershop, customers
 
 WHERE supershop.customer_id = customers.Customer_ID
+
+
+-- inner join ..............................
+
+SELECT
+customer_name,
+age,
+gender,
+country,
+region
+
+FROM supershop
+INNER JOIN customers on supershop.customer_id = customers.customer_id
